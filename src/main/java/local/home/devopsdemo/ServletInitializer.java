958544ae98850276f43de00bc9460a6907cc8c3b
Tejinder Gill <tej_gill24@yahoo.com>
2020-02-06 20:52:24 -0500

    java code

---
+++ src/main/java/local/home/devopsdemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package local.home.devopsdemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DevopsdemoApplication.class);
+	}
+
+}
